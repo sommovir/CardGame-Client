2c1f4a6229c8f24c16c78ca08394f7d8543d6973
Luca <Luca@DESKTOP-HG275RN>
2020-08-08 11:46:29 +0200

    init

--- src/main/java/it/lule/cardgame/client/mqtt/MQTTClient.java
+++ src/main/java/it/lule/cardgame/client/mqtt/MQTTClient.java
@@ -6,6 +6,7 @@
 package it.lule.cardgame.client.mqtt;
 
 import it.lule.cardgame.client.logic.EventManager;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
@@ -25,7 +26,13 @@ public class MQTTClient implements MqttCallback {
     private static MQTTClient _instance = null;
     private final int qos = 2;
     private MqttClient sampleClient = null;
-    
+
+    String content = "Visit www.hascode.com! :D";
+
+    String broker = "tcp://0.0.0.0:1883";
+
+    String clientId = "paho-java-client"+new Date().getTime();
+
     public static MQTTClient getInstance() {
         if (_instance == null) {
             _instance = new MQTTClient();
@@ -41,11 +48,6 @@ public class MQTTClient implements MqttCallback {
 
     public void connect() {
         String topic = "news";
-        String content = "Visit www.hascode.com! :D";
-
-        String broker = "tcp://0.0.0.0:1883";
-
-        String clientId = "paho-java-client";
 
         try {
             sampleClient = new MqttClient(broker, clientId, new MemoryPersistence());
@@ -77,6 +79,16 @@ public class MQTTClient implements MqttCallback {
         try {
             MqttMessage mx = new MqttMessage(message.getBytes());
             mx.setQos(qos);
+            if (sampleClient.isConnected()) {
+                System.out.println("CONNESSOOOO");
+            } else {
+                sampleClient = new MqttClient(broker, clientId, new MemoryPersistence());
+                MqttConnectOptions connOpts = new MqttConnectOptions();
+                connOpts.setCleanSession(true);
+                System.out.println("paho-client connecting to broker: " + broker);
+                sampleClient.connect(connOpts);
+                System.out.println("NOT CONNESSO");
+            }
             sampleClient.publish(topic, mx);
         } catch (MqttException ex) {
             Logger.getLogger(MQTTClient.class.getName()).log(Level.SEVERE, null, ex);
@@ -85,11 +97,13 @@ public class MQTTClient implements MqttCallback {
 
     @Override
     public void connectionLost(Throwable thrwbl) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
     public void messageArrived(String topic, MqttMessage mm) throws Exception {
+        System.out.println("TOPIC: "+topic);
+        System.out.println("MESSAGE: "+new String(mm.getPayload()));
         if (topic.equals("UserConnected")) {
             EventManager.getInstance().userConnected(new String(mm.getPayload()));
         }
@@ -97,7 +111,7 @@ public class MQTTClient implements MqttCallback {
 
     @Override
     public void deliveryComplete(IMqttDeliveryToken imdt) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
 }
